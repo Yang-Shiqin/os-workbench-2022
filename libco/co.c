cca90471be6b61f511c650a13931372f1998358c
tracer-nju <tracer@nju.edu.cn>
2023-01-14 10:09:17 +0800

    Linux DESKTOP-1F60OM8 4.4.0-19041-Microsoft #2311-Microsoft Tue Nov 08 17:09:00 PST 2022 x86_64 x86_64 x86_64 GNU/Linux  10:09:18 up 1 day, 23:36,  0 users,  load average: 0.52, 0.58, 0.59

--- libco/co.c
+++ libco/co.c
@@ -1,15 +1,66 @@
 #include "co.h"
 #include <stdlib.h>
+#include <string.h>
+#include <ucontext.h>
 
 struct co {
+    char state;
+    char name[8];
+    ucontext_t ucp;
+    ucontext_t ucp_end;
+    char stack[1024];
 };
 
+struct co* list[128]={0};
+int next=0;
+int now=0;
+struct co end;
+
+void co_end(int i){
+    list[i]->state = 0;
+}
+
 struct co *co_start(const char *name, void (*func)(void *), void *arg) {
-  return NULL;
+    ucontext_t context;
+    struct co* ret = malloc(sizeof(struct co));
+    list[next] = ret;
+    now = next;
+    while(NULL!=list[next]){
+        next = (next+1)%128;
+    }
+
+    ret->state = 1; // 开始
+    getcontext(&(ret->ucp_end));
+    makecontext(&(ret->ucp_end), co_end, 1, now);
+    strcpy(ret->name, name);
+    ret->ucp.uc_stack.ss_sp = ret->stack;
+    ret->ucp.uc_stack.ss_size = sizeof(ret->stack); // 栈大小
+    ret->ucp.uc_link = &(ret->ucp_end); 
+    getcontext(&(ret->ucp));
+    makecontext(&(ret->ucp), func, 1, arg); // 指定待执行的函数入口
+    getcontext(&context);
+    context.uc_link = &(ret->ucp);
+    setcontext(&context);
+    return ret;
 }
 
 void co_wait(struct co *co) {
+    while(NULL!=co && 1==co->state){
+        co_yield();
+    }
+    if(NULL!=co){
+        free(co);
+        co = NULL;
+        list[now]=NULL;
+    }    
 }
 
 void co_yield() {
+    int i = rand() % 128;
+    while(NULL==list[i]){
+        i = rand() % 128;
+    }
+    int tmp=now;
+    now = i;
+    swapcontext(&(list[tmp]->ucp), &(list[i]->ucp));
 }
