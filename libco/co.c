3716b2c674f341301a494811d4227743faf60bd0
tracer-nju <tracer@nju.edu.cn>
2023-01-16 16:16:20 +0800

    Linux DESKTOP-1F60OM8 4.4.0-19041-Microsoft #2311-Microsoft Tue Nov 08 17:09:00 PST 2022 x86_64 x86_64 x86_64 GNU/Linux  16:16:20 up 4 days,  5:43,  0 users,  load average: 0.52, 0.58, 0.59

--- libco/co.c
+++ libco/co.c
@@ -2,7 +2,9 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
-#include <ucontext.h>
+#include <setjmp.h>
+
+#define STACK_SIZE 2048
 
 #ifndef LOCAL_MACHINE
   #define debug(...) printf(__VA_ARGS__)
@@ -10,27 +12,39 @@
   #define debug()
 #endif
 
-struct co {
-    char state;
-    char name[16];
-    ucontext_t ucp;
-    ucontext_t ucp_end;
-    ucontext_t ucp_sta;
-    char stack[8192];
-    char stack_end[8192];
-    char stack_sta[8192];
+static inline void stack_switch_call(void *sp, void *entry, uintptr_t arg) {
+  asm volatile (
+#if __x86_64__
+    "movq %0, %%rsp; movq %2, %%rdi; jmp *%1"
+      : : "b"((uintptr_t)sp), "d"(entry), "a"(arg) : "memory"
+#else
+    "movl %0, %%esp; movl %2, 4(%0); jmp *%1"
+      : : "b"((uintptr_t)sp - 8), "d"(entry), "a"(arg) : "memory"
+#endif
+  );
+}
+
+enum co_status {
+  CO_NEW = 1, // 新创建，还未执行过
+  CO_RUNNING, // 已经执行过
+  CO_WAITING, // 在 co_wait 上等待
+  CO_DEAD,    // 已经结束，但还未释放资源
 };
 
-static struct co* list[128]={0};
-static int next=0;
-static int now=0;
-static int max=0;
-static struct co end;
+struct co {
+    enum co_status state;       // 协程状态
+    char* name;                 // 协程名字
+    jmp_buf env;                // 保存上下文
+    uint8_t stack[STACK_SIZE];  // 协程的堆栈
+    void (*func)(void *);       // co_start 指定的入口地址和参数
+    void *arg;    
+    struct co* waiter;          // 是否有其他协程在等待当前协程
+};
 
-void co_end(int i){
-    list[i]->state = 0;
-    debug("end\n");
-}
+struct co* list[128]={0};
+int now=0;
+int next=0;
+int max=0;
 
 struct co *co_start(const char *name, void (*func)(void *), void *arg) {
     debug("start\n");
@@ -42,33 +56,21 @@ struct co *co_start(const char *name, void (*func)(void *), void *arg) {
         max = next>max?next:max;
     }
 
-    ret->state = 1; // 开始
-    getcontext(&(ret->ucp_end));
-    getcontext(&(ret->ucp));
-    getcontext(&(ret->ucp_sta));
-    ret->ucp_end.uc_stack.ss_sp = ret->stack_end;
-    ret->ucp_end.uc_stack.ss_size = sizeof(ret->stack_end); // 栈大小
-    ret->ucp_end.uc_link = &(ret->ucp_sta);
-    makecontext(&(ret->ucp_end), (void (*)(void))co_end, 1, now);
-    strcpy(ret->name, name);
-    ret->ucp.uc_stack.ss_sp = ret->stack;
-    ret->ucp.uc_stack.ss_size = sizeof(ret->stack); // 栈大小
-    ret->ucp.uc_link = &(ret->ucp_end); 
-    ret->ucp_sta.uc_stack.ss_sp = ret->stack_sta;
-    ret->ucp_sta.uc_stack.ss_size = sizeof(ret->stack_sta); // 栈大小
-    ret->ucp_sta.uc_link = NULL;
-    makecontext(&(ret->ucp), (void (*)(void))func, 1, arg); // 指定待执行的函数入口
-    debug("before set\n");
-    swapcontext(&(ret->ucp_sta), &(ret->ucp));
-    debug("after set\n");
+    ret->state = CO_NEW; // 开始
+    ret->name = name;
+    ret->func = func;
+    ret->arg = arg;
+    ret->waiter = NULL;
+    func(arg);
     return ret;
 }
 
 void co_wait(struct co *co) {
-    while(NULL!=co && 1==co->state){
+    while(NULL!=co && CO_DEAD!=co->state){
         co_yield();
     }
     if(NULL!=co){
+        // 不对，我咋知道后面有没有人等我
         free(co);
         co = NULL;
         list[now]=NULL;
@@ -84,5 +86,11 @@ void co_yield() {
     int tmp=now;
     debug("%d, %d, %d\n", now, i, max);
     now = i;
-    swapcontext(&(list[tmp]->ucp), &(list[i]->ucp));
+    int val = setjmp(list[tmp]->env);
+    if (val == 0) {
+        tack_switch_call(list[now]->stack, list[now]->func, list[now]->arg);
+        longjmp(list[now]->env, 0);
+    } else {
+        ;
+    }
 }
