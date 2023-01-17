91a0a94f48502dce416e9d08a2cab07aa08a2300
tracer-nju <tracer@nju.edu.cn>
2023-01-17 14:49:00 +0800

    Linux DESKTOP-1F60OM8 4.4.0-19041-Microsoft #2311-Microsoft Tue Nov 08 17:09:00 PST 2022 x86_64 x86_64 x86_64 GNU/Linux  14:49:00 up 5 days,  4:16,  0 users,  load average: 0.52, 0.58, 0.59

--- libco/co.c
+++ libco/co.c
@@ -88,11 +88,12 @@ void co_yield() {
             && (list[i]->state!=CO_NEW))){
             i = rand() % (max+1);
         }
-        if(i==now) return;
+        // if(i==now) return;
         int last=now;
         now = i;
         if(list[now]->state==CO_NEW){
             list[now]->state=CO_RUNNING;
+            // 寄存器从高向低生长
             stack_switch_call(list[now]->stack+STACK_SIZE, list[now]->func, (uintptr_t)list[now]->arg);   // 切换栈，在自己的栈上运行函数
             // 函数运行完
             list[now]->state = CO_DEAD;
