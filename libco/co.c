1be7c86c2d6963625f21e2eb8982d509dc40ce36
tracer-nju <tracer@nju.edu.cn>
2023-01-17 17:17:57 +0800

    Linux DESKTOP-1F60OM8 4.4.0-19041-Microsoft #2311-Microsoft Tue Nov 08 17:09:00 PST 2022 x86_64 x86_64 x86_64 GNU/Linux  17:17:57 up 5 days,  6:45,  0 users,  load average: 0.52, 0.58, 0.59

--- libco/co.c
+++ libco/co.c
@@ -9,6 +9,10 @@
 
 #define STACK_SIZE 8192
 #define LIST_SIZE 128
+static struct co* list[LIST_SIZE]={0};
+static int next=0;
+static int now=0;
+static int max=0;
 
 struct co *current;
 
@@ -30,54 +34,54 @@ struct co {
   unsigned char stack[STACK_SIZE];  // 协程的堆栈
 };
 
-typedef struct CONODE {
-  struct co *coroutine;
+// typedef struct CONODE {
+//   struct co *coroutine;
 
-  struct CONODE *fd, *bk;
-} CoNode;
+//   struct CONODE *fd, *bk;
+// } CoNode;
 
-static CoNode *co_node = NULL;
-/*
- * 如果co_node == NULL，则创建一个新的双向循环链表即可，并返回
- * 如果co_node != NULL, 则在co_node和co_node->fd之间插入，仍然返回co_node的值
- */
-static void co_node_insert(struct co *coroutine) {
-  CoNode *victim = (CoNode *)malloc(sizeof(CoNode));
-  assert(victim);
+// static CoNode *co_node = NULL;
+// /*
+//  * 如果co_node == NULL，则创建一个新的双向循环链表即可，并返回
+//  * 如果co_node != NULL, 则在co_node和co_node->fd之间插入，仍然返回co_node的值
+//  */
+// static void co_node_insert(struct co *coroutine) {
+//   CoNode *victim = (CoNode *)malloc(sizeof(CoNode));
+//   assert(victim);
 
-  victim->coroutine = coroutine;
-  if (co_node == NULL) {
-    victim->fd = victim->bk = victim;
-    co_node = victim;
-  } else {
-    victim->fd = co_node->fd;
-    victim->bk = co_node;
-    victim->fd->bk = victim->bk->fd = victim;
-  }
-}
+//   victim->coroutine = coroutine;
+//   if (co_node == NULL) {
+//     victim->fd = victim->bk = victim;
+//     co_node = victim;
+//   } else {
+//     victim->fd = co_node->fd;
+//     victim->bk = co_node;
+//     victim->fd->bk = victim->bk->fd = victim;
+//   }
+// }
 
-/*
- * 如果当前只剩node一个，则返回该一个
- * 否则，拉取当前co_node对应的协程，并沿着bk方向移动
- */
-static CoNode *co_node_remove() {
-  CoNode *victim = NULL;
+// /*
+//  * 如果当前只剩node一个，则返回该一个
+//  * 否则，拉取当前co_node对应的协程，并沿着bk方向移动
+//  */
+// static CoNode *co_node_remove() {
+//   CoNode *victim = NULL;
 
-  if (co_node == NULL) {
-    return NULL;
-  } else if (co_node->bk == co_node) {
-    victim = co_node;
-    co_node = NULL;
-  } else {
-    victim = co_node;
+//   if (co_node == NULL) {
+//     return NULL;
+//   } else if (co_node->bk == co_node) {
+//     victim = co_node;
+//     co_node = NULL;
+//   } else {
+//     victim = co_node;
 
-    co_node = co_node->bk;
-    co_node->fd = victim->fd;
-    co_node->fd->bk = co_node;
-  }
+//     co_node = co_node->bk;
+//     co_node->fd = victim->fd;
+//     co_node->fd->bk = co_node;
+//   }
 
-  return victim;
-}
+//   return victim;
+// }
 
 struct co *co_start(const char *name, void (*func)(void *), void *arg) {
   struct co *coroutine = (struct co *)malloc(sizeof(struct co));
@@ -89,7 +93,12 @@ struct co *co_start(const char *name, void (*func)(void *), void *arg) {
   coroutine->status = CO_NEW;
   coroutine->waiter = NULL;
 
-  co_node_insert(coroutine);
+    list[next] = coroutine;
+    while(NULL!=list[next]){
+        next = (next+1)%LIST_SIZE;
+        max = next>max?next:max;
+    }
+//   co_node_insert(coroutine);
   return coroutine;
 }
 
@@ -97,22 +106,29 @@ void co_wait(struct co *coroutine) {
   assert(coroutine);
 
   if (coroutine->status != CO_DEAD) {
-    coroutine->waiter = current;
-    current->status = CO_WAITING;
+    coroutine->waiter = list[now];
+    list[now]->status = CO_WAITING;
     co_yield ();
   }
 
   /*
    * 释放coroutine对应的CoNode
    */
-  while (co_node->coroutine != coroutine) {
-    co_node = co_node->bk;
-  }
+//   while (co_node->coroutine != coroutine) {
+//     co_node = co_node->bk;
+//   }
 
-  assert(co_node->coroutine == coroutine);
+//   assert(co_node->coroutine == coroutine);
 
-  free(coroutine);
-  free(co_node_remove());
+//   free(coroutine);
+//   free(co_node_remove());
+    int i;
+    for(i=0; i<LIST_SIZE && list[i]!=coroutine; i++){;}
+    if(list[i]==coroutine){
+        free(coroutine);
+        coroutine = NULL;
+        list[i]=NULL;
+    }
 }
 
 /*
@@ -134,51 +150,55 @@ static inline void stack_switch_call(void *sp, void *entry, void *arg) {
 
 #define __LONG_JUMP_STATUS (1)
 void co_yield () {
-  int status = setjmp(current->context);
+  int status = setjmp(list[now]->context);
   if (!status) {
-    //此时开始查找待选中的进程，因为co_node应该指向的就是current对应的节点，因此首先向下移动一个，使当前线程优先级最低
-    co_node = co_node->bk;
-    while (!((current = co_node->coroutine)->status == CO_NEW || current->status == CO_RUNNING)) {
-      co_node = co_node->bk;
+    int i = rand() % (max+1);
+    while((NULL==list[i]) ||
+    ((list[i]->state!=CO_RUNNING) && (list[i]->state!=CO_NEW))){
+        i = rand() % (max+1);
     }
+    now = i;
+    //此时开始查找待选中的进程，因为co_node应该指向的就是current对应的节点，因此首先向下移动一个，使当前线程优先级最低
+    // co_node = co_node->bk;
+    // while (!((current = co_node->coroutine)->status == CO_NEW || current->status == CO_RUNNING)) {
+    //   co_node = co_node->bk;
+    // }
 
-    assert(current);
+    assert(list[now]);
 
-    if (current->status == CO_RUNNING) {
-      longjmp(current->context, __LONG_JUMP_STATUS);
+    if (list[now]->status == CO_RUNNING) {
+      longjmp(list[now]->context, __LONG_JUMP_STATUS);
     } else {
-      ((struct co volatile *)current)->status = CO_RUNNING;  //这里如果直接赋值，编译器会和后面的覆写进行优化
+      ((struct co volatile *)list[now])->status = CO_RUNNING;  //这里如果直接赋值，编译器会和后面的覆写进行优化
 
       // 栈由高地址向低地址生长
-      stack_switch_call(current->stack + STACK_SIZE, current->func, current->arg);
+      stack_switch_call(list[now]->stack + STACK_SIZE, list[now]->func, list[now]->arg);
       //恢复相关寄存器
 
       //此时协程已经完成执行
-      current->status = CO_DEAD;
+      list[now]->status = CO_DEAD;
 
-      if (current->waiter) {
-        current->waiter->status = CO_RUNNING;
+      if (list[now]->waiter) {
+        list[now]->waiter->status = CO_RUNNING;
       }
       co_yield ();
     }
   }
 
-  assert(status && current->status == CO_RUNNING);  //此时一定是选中的进程通过longjmp跳转到的情况执行到这里
+  assert(status && list[now]->status == CO_RUNNING);  //此时一定是选中的进程通过longjmp跳转到的情况执行到这里
 }
 
 static __attribute__((constructor)) void co_constructor(void) {
-  current = co_start("main", NULL, NULL);
-  current->status = CO_RUNNING;
+  list[now] = co_start("main", NULL, NULL);
+  list[now]->status = CO_RUNNING;
 }
 
 static __attribute__((destructor)) void co_destructor(void) {
-  if (co_node == NULL) {
-    return;
-  }
-
-  while (co_node) {
-    current = co_node->coroutine;
-    free(current);
-    free(co_node_remove());
-  }
+    int i;
+    for(i=0; i<LIST_SIZE; i++){
+        if(list[i]!=NULL){
+            free(list[i]);
+            list[i]=NULL;
+        }
+    }
 }
