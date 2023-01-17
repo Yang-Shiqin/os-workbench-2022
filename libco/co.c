4b144afcba92fed10a4707b886729a31995868b4
tracer-nju <tracer@nju.edu.cn>
2023-01-17 18:45:23 +0800

    Linux DESKTOP-1F60OM8 4.4.0-19041-Microsoft #2311-Microsoft Tue Nov 08 17:09:00 PST 2022 x86_64 x86_64 x86_64 GNU/Linux  18:45:23 up 5 days,  8:12,  0 users,  load average: 0.52, 0.58, 0.59

--- libco/co.c
+++ libco/co.c
@@ -99,6 +99,7 @@ void co_yield() {
             list[now]->state = CO_DEAD;
             if(list[now]->waiter)
                 list[now]->waiter->state = CO_RUNNING;
+            co_yield();
         }else{
             longjmp(list[now]->env, 1);
         }
