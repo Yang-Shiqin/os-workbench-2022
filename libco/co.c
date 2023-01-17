2e3032e75f3202805b6349e118fe56836132ff8f
tracer-nju <tracer@nju.edu.cn>
2023-01-17 15:05:32 +0800

    Linux DESKTOP-1F60OM8 4.4.0-19041-Microsoft #2311-Microsoft Tue Nov 08 17:09:00 PST 2022 x86_64 x86_64 x86_64 GNU/Linux  15:05:32 up 5 days,  4:32,  0 users,  load average: 0.52, 0.58, 0.59

--- libco/co.c
+++ libco/co.c
@@ -24,22 +24,22 @@ static inline void stack_switch_call(void *sp, void *entry, uintptr_t arg) {
 #endif
   );
 }
-/*
- * 切换栈，即让选中协程的所有堆栈信息在自己的堆栈中，而非调用者的堆栈。保存调用者需要保存的寄存器，并调用指定的函数
- */
-static inline void stack_switch_call(void *sp, void *entry, uintptr_t arg) {
-  asm volatile(
-#if __x86_64__
-      "movq %%rcx, 0(%0); movq %0, %%rsp; movq %2, %%rdi; call *%1"
-      :
-      : "b"((uintptr_t)sp - 16), "d"((uintptr_t)entry), "a"((uintptr_t)arg)
-#else
-      "movl %%ecx, 4(%0); movl %0, %%esp; movl %2, 0(%0); call *%1"
-      :
-      : "b"((uintptr_t)sp - 8), "d"((uintptr_t)entry), "a"((uintptr_t)arg)
-#endif
-  );
-}
+// /*
+//  * 切换栈，即让选中协程的所有堆栈信息在自己的堆栈中，而非调用者的堆栈。保存调用者需要保存的寄存器，并调用指定的函数
+//  */
+// static inline void stack_switch_call(void *sp, void *entry, uintptr_t arg) {
+//   asm volatile(
+// #if __x86_64__
+//       "movq %%rcx, 0(%0); movq %0, %%rsp; movq %2, %%rdi; call *%1"
+//       :
+//       : "b"((uintptr_t)sp - 16), "d"((uintptr_t)entry), "a"((uintptr_t)arg)
+// #else
+//       "movl %%ecx, 4(%0); movl %0, %%esp; movl %2, 0(%0); call *%1"
+//       :
+//       : "b"((uintptr_t)sp - 8), "d"((uintptr_t)entry), "a"((uintptr_t)arg)
+// #endif
+//   );
+// }
 /*
  * 从调用的指定函数返回，并恢复相关的寄存器。此时协程执行结束，以后再也不会执行该协程的上下文。这里需要注意的是，其和上面并不是对称的，因为调用协程给了新创建的选中协程的堆栈，则选中协程以后就在自己的堆栈上执行，永远不会返回到调用协程的堆栈。
  */
