8dec7827a21c35d5cb66c91e86e227d9d3d63a4e
tracer-nju <tracer@nju.edu.cn>
2023-01-17 15:35:04 +0800

    Linux DESKTOP-1F60OM8 4.4.0-19041-Microsoft #2311-Microsoft Tue Nov 08 17:09:00 PST 2022 x86_64 x86_64 x86_64 GNU/Linux  15:35:04 up 5 days,  5:02,  0 users,  load average: 0.52, 0.58, 0.59

--- libco/co.c
+++ libco/co.c
@@ -121,7 +121,7 @@ void co_wait(struct co *coroutine) {
 static inline void stack_switch_call(void *sp, void *entry, void *arg) {
   asm volatile(
 #if __x86_64__
-      "movq %%rcx, 0(%0); movq %0, %%rsp; movq %2, %%rdi; call *%1"
+      "movq %0, %%rsp; movq %2, %%rdi; call *%1"
       :
       : "b"((uintptr_t)sp - 16), "d"((uintptr_t)entry), "a"((uintptr_t)arg)
 #else
@@ -137,7 +137,7 @@ static inline void stack_switch_call(void *sp, void *entry, void *arg) {
 static inline void restore_return() {
   asm volatile(
 #if __x86_64__
-      "movq 0(%%rsp), %%rcx"
+      ""
       :
       :
 #else
