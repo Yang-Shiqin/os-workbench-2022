25f8a007f17e28da648bff8d6a4d19b8584cfdc0
tracer-nju <tracer@nju.edu.cn>
2023-01-17 16:26:04 +0800

    Linux DESKTOP-1F60OM8 4.4.0-19041-Microsoft #2311-Microsoft Tue Nov 08 17:09:00 PST 2022 x86_64 x86_64 x86_64 GNU/Linux  16:26:04 up 5 days,  5:53,  0 users,  load average: 0.52, 0.58, 0.59

--- libco/co.c
+++ libco/co.c
@@ -36,11 +36,11 @@ enum co_status {
 struct co {
     enum co_status state;
     const char* name;
-    jmp_buf env;
-    unsigned char stack[STACK_SIZE];  // 栈太小会segmentation fault
     void (*func)(void *); // co_start 指定的入口地址和参数
     void *arg;
     struct co* waiter;
+    jmp_buf env;
+    unsigned char stack[STACK_SIZE];  // 栈太小会segmentation fault
 };
   
 static struct co* list[LIST_SIZE]={0};
