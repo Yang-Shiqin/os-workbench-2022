bc6deec1a60dac02d92c205a5f409ab877249eae
tracer-nju <tracer@nju.edu.cn>
2024-06-23 14:58:20 +0800

    Linux ysq 4.4.0-22621-Microsoft #3672-Microsoft Fri Jan 01 08:00:00 PST 2016 x86_64 x86_64 x86_64 GNU/Linux  14:58:20 up 7 days,  6:44,  0 users,  load average: 0.52, 0.58, 0.59

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -1,18 +1,27 @@
 #include <common.h>
 
-// 空闲内存链表
-// struct area_linklist{
-//   Area area;
-//   struct area_linklist * next;
-// }node={
-//   .area = Area
-// };
+// spinlock_t lock = spin_init("Big Kernel Lock");
+
+// 空闲内存链表（改成数组记录不同大小的内存链表，然后里面是类似大小的内存链表？）
+typedef struct FreeNode{
+  uintptr_t pmsize;
+  struct FreeNode * next;
+  // TODO: 锁(不知道有没有现成的，还是得自己实现)
+}FreeNode;
 
 static void *kalloc(size_t size) {
+  // 全局锁方案
+  // spin_lock(&lock);
+  // // 申请内存
+  // spin_unlock(&lock);
+  // 申请当前节点的锁
+  // 检查大小是否够(要从特定位置开始读), 够则分裂, 剩下更新到空闲(前面的整个申请, 但返回整的地址; 还是前面空闲的也加到链表中)
+  // 不够申请下一节点锁, 更新完才释放当前节点锁
   return NULL;
 }
 
 static void kfree(void *ptr) {
+  // 合并连续空闲
 }
 
 static void pmm_init() {
