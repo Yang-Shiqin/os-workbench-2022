096e58d308fd389e99f1a786cb04fca2b495dbf3
tracer-nju <tracer@nju.edu.cn>
2024-06-25 09:36:40 +0800

    Linux ysq 4.4.0-22621-Microsoft #3672-Microsoft Fri Jan 01 08:00:00 PST 2016 x86_64 x86_64 x86_64 GNU/Linux  09:36:40 up 17:03,  0 users,  load average: 0.52, 0.58, 0.59

--- kernel/framework/main.c
+++ kernel/framework/main.c
@@ -1,8 +1,34 @@
 #include <kernel.h>
 #include <klib.h>
+typedef struct Header { 
+	size_t size; // Block size, 不加头
+	int magic; // canary, 0xfdfdfdfd为正常
+} Header;
+
+void stress_test() {
+                void *ptr = pmm->alloc(30);
+                Header* h = ptr-sizeof(Header);
+                printf("%d %d\n", h->size, h->magic);
+
+    // while (1) {
+    //     // 根据 workload 生成操作
+    //     struct malloc_op op = random_op();
+
+    //     switch (op.type) {
+    //         case OP_ALLOC: {
+    //             void *ptr = pmm->alloc(op.sz);
+    //             alloc_check(ptr, op.sz);
+    //             break;
+    //         }
+    //         case OP_FREE:
+    //             free(op.addr);
+    //             break;
+    //     }
+    // }
+}
 
 int main() {
   os->init();
-  mpe_init(os->run);
+  mpe_init(stress_test);
   return 1;
 }
