65974cda0069054f5de174640899e2971eec6669
tracer-nju <tracer@nju.edu.cn>
2024-06-28 17:02:55 +0800

    Linux ysq 4.4.0-22621-Microsoft #3672-Microsoft Fri Jan 01 08:00:00 PST 2016 x86_64 x86_64 x86_64 GNU/Linux  17:02:55 up 1 day,  8:38,  0 users,  load average: 0.52, 0.58, 0.59

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -26,7 +26,9 @@ int strace(int fd, int argc, char *argv[]){
   }
   char *exec_envp[] = { "PATH=/bin:/usr/bin", NULL, };
   dup2(fd, STDERR_FILENO);
+  fflush(STDOUT_FILENO);
   close(STDOUT_FILENO);
+  fflush(STDERR_FILENO);
   execve("/bin/strace", exec_argv, exec_envp);
   perror("execve");
   exit(EXIT_FAILURE);
