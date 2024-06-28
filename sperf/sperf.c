6d25e9327c0f4388f781fb7cc7c61a72ebc73a33
tracer-nju <tracer@nju.edu.cn>
2024-06-28 16:26:55 +0800

    Linux ysq 4.4.0-22621-Microsoft #3672-Microsoft Fri Jan 01 08:00:00 PST 2016 x86_64 x86_64 x86_64 GNU/Linux  16:26:55 up 1 day,  8:02,  0 users,  load average: 0.52, 0.58, 0.59

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -13,58 +13,64 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-// // [ ] todo
-// // 子进程调用strace CMD [ARG], 输出管道给父进程
-// int strace(int fd){
-//   char *exec_argv[] = { "strace", "ls", NULL, };
-//   char *exec_envp[] = { "PATH=/bin", NULL, };
-//   execve("/bin/strace",     exec_argv, exec_envp);
-//   return 0;
-// }
+// [ ] todo
+// 子进程调用strace CMD [ARG], 输出管道给父进程
+int strace(int fd){
+  char *exec_argv[] = { "strace", "ls", NULL, };
+  char *exec_envp[] = { "PATH=/bin", NULL, };
+  dup2(fd, STDOUT_FILENO);
+  execve("/bin/strace",     exec_argv, exec_envp);
+  return 0;
+}
 
-// // [ ] todo
-// // 父进程读取管道输入, 解析并统计syscall时长, 展示出来
-// int sperf(int fd){
-//   char buf[257];
-//   return 0;
-// }
+// [ ] todo
+// 父进程读取管道输入, 解析并统计syscall时长, 展示出来
+int sperf(int fd){
+  char buf[1024];
+  while (read(fd, buf, sizeof(buf)-1) > 0){
+    // 解析并显示时间
+    printf("[%d] Got: '%s'\n", getpid(), buf);
+  }
+  close(fd);
+  return 0;
+}
 
 // [ ] todo
 // sperf CMD [ARG]  ->  strace CMD [ARG] | sperf
 int main(int argc, char *argv[]) {
-  // // 1. 判断调用形式
-  // if (argc<2){
-  //   fprintf(stderr, "Usage: %s <CMD> [ARG]\n", argv[0]);
-  //   exit(EXIT_FAILURE);
-  // }
-  // // 3. 创建管道
-  // int pipefd[2];
-  // if (-1==pipe(pipefd)){
-  //   perror("pipe");
-  //   exit(EXIT_FAILURE);
-  // }
-  // // 4. fork进程
-  // pid_t pid = fork();
-  // if (-1==pid){
-  //   perror("fork");
-  //   exit(EXIT_FAILURE);
-  // }else if (0==pid){
-  //   // child: 关闭读口
-  //   close(pipefd[0]);
-  //   strace(pipefd[1]);
-  // }else{
-  //   // parent: 关闭写口
-  //   close(pipefd[1]);
-  //   sperf(pipefd[0]);
-  // }
+  // 1. 判断调用形式
+  if (argc<2){
+    fprintf(stderr, "Usage: %s <CMD> [ARG]\n", argv[0]);
+    exit(EXIT_FAILURE);
+  }
+  // 3. 创建管道
+  int pipefd[2];
+  if (-1==pipe(pipefd)){
+    perror("pipe");
+    exit(EXIT_FAILURE);
+  }
+  // 4. fork进程
+  pid_t pid = fork();
+  if (-1==pid){
+    perror("fork");
+    exit(EXIT_FAILURE);
+  }else if (0==pid){
+    // child: 关闭读口
+    close(pipefd[0]);
+    strace(pipefd[1]);
+  }else{
+    // parent: 关闭写口
+    close(pipefd[1]);
+    sperf(pipefd[0]);
+  }
 
 
 
 
-  char *exec_argv[] = { /*"strace",*/ "ls", NULL, };
-  char *exec_envp[] = { "PATH=/bin", NULL, };
+  // char *exec_argv[] = { "strace", "ls", NULL, };
+  // char *exec_envp[] = { "PATH=/bin", NULL, };
   // execve("strace",          exec_argv, exec_envp);
-  execve("/bin/strace",     exec_argv, exec_envp);
+  // execve("/bin/strace",     exec_argv, exec_envp);
   // execve("/usr/bin/strace", exec_argv, exec_envp);
   perror(argv[0]);
   exit(EXIT_FAILURE);
