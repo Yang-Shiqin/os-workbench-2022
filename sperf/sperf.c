c9a9db2cf74d782dc274c43e64a2a19e55e80203
tracer-nju <tracer@nju.edu.cn>
2024-06-29 17:46:33 +0800

    Linux ysq 4.4.0-22621-Microsoft #3672-Microsoft Fri Jan 01 08:00:00 PST 2016 x86_64 x86_64 x86_64 GNU/Linux  17:46:33 up 2 days,  9:21,  0 users,  load average: 0.52, 0.58, 0.59

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -150,14 +150,7 @@ int sperf(int fd){
         pbuf += matches[0].rm_eo+1;
         assert(*(pbuf)!='\n');
       } else if (ret == REG_NOMATCH) {
-        // 到最后了
-        if (*(pbuf2-1)=='?' || *(pbuf2+1)=='+') break;
-        printf("pbuf2-1:%s\n", (pbuf2-1));
-        printf("pbuf2:%s\n", (pbuf2));
-        printf("pbuf:%s\n", (pbuf));
-        printf("remove_buf:%s\n", (remove_buf[0]));
-        printf("%c\n", *(pbuf2-1));
-        // assert(*(pbuf2-1)=='?');
+        break;
       } else {
         perror("regex");
         exit(EXIT_FAILURE);
