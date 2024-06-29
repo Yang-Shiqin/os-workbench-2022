2214b1b9933e84cf5162701d3af771ce6021c74a
tracer-nju <tracer@nju.edu.cn>
2024-06-29 10:00:56 +0800

    Linux ysq 4.4.0-22621-Microsoft #3672-Microsoft Fri Jan 01 08:00:00 PST 2016 x86_64 x86_64 x86_64 GNU/Linux  10:00:56 up 2 days,  1:36,  0 users,  load average: 0.52, 0.58, 0.59

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -1,17 +1,16 @@
-/******** todo
- * execve怎么用, 环境变量怎么处理(读手册)
- * strace二进制文件位置
- ************* done
- * pipe怎么用
- * fork用法
- * 时间统计
- * CMD带/直接执行, 否则在PATH中查找, 的实现
- */
-
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
+
+// [ ] todo: name可能超, 目前用的是栈上数组, 也可能超
+typedef struct SyscallInfo{
+    char name[16];  // syscall name
+    double time;    // add up duration(s)
+    // struct SyscallInfo * next;
+} SyscallInfo;
+
+
 
 // 子进程调用strace CMD [ARG], 输出管道给父进程
 int strace(int fd, int argc, char *argv[]){
@@ -19,13 +18,13 @@ int strace(int fd, int argc, char *argv[]){
   int i;
   // 构建参数
   exec_argv[0] = "strace";
-  exec_argv[1] = "-T";
+  exec_argv[1] = "--syscall-times=ns";
   exec_argv[argc+2] = NULL;
   for (i=0; i<argc; i++){
     exec_argv[i+2] = argv[i];
   }
   // execve会自动在环境变量中查找
-  char *exec_envp[] = { "PATH=/sd:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin", NULL, };
+  char *exec_envp[] = { "PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin", NULL, };
   dup2(fd, STDERR_FILENO);
   close(STDOUT_FILENO);
   execve("/bin/strace", exec_argv, exec_envp);
@@ -35,20 +34,101 @@ int strace(int fd, int argc, char *argv[]){
 
 // 父进程读取管道输入, 解析并统计syscall时长, 展示出来
 int sperf(int fd){
-  char buf[256]={0};
+  char buf[256] = {0};
+  char *pbuf = buf;
+  char name_buf[16] = {0};  // \nsyscall_name(
+  char time_buf[16] = {0};  // <syscall_time>
+  int name_tail=0, time_tail=0;
+  double time=0;
+  int reading_name = 1, reading_time = 0, reading = 1; // 1是正在读取(read没读完整), 0是没有正在读取
+  SyscallInfo syscall_info_list[512] = {0};
+  int tail=0;
   ssize_t num_read;
   while ((num_read=read(fd, buf, sizeof(buf)-1)) > 0){
-// [ ] todo
+    pbuf = buf;
     // 解析并显示时间
     buf[num_read] = 0;
     printf("%s", buf);
+    while(pbuf<buf+num_read){
+      assert(!(reading_name&reading_time&1)); // 不能同时正在读取
+      assert((!reading)&&(reading_name|reading_time));
+      int i;
+      if (reading_name){// 读syscall name
+        i = strstr(pbuf, "(");
+        if(i){  // 读完syscall name
+          pbuf[i] = 0;
+          strcat(name_buf, pbuf);
+          reading_name = 0;
+          pbuf += i+1;
+        }else{  // syscall name中断
+          strcat(name_buf, pbuf);
+          pbuf = buf+num_read;
+        }
+      }else if (reading_time){
+          i = strstr(pbuf, ">");
+          if (!i){  // syscall time中断
+            strcat(time_buf, pbuf);
+            pbuf = buf+num_read;
+          }else{  // 读完一条syscall
+            reading_time = 0;
+            pbuf[i] = 0;
+            strcat(time_buf, pbuf);
+            int list_i;
+            double tmp_time = atof(time_buf);
+            time += tmp_time;
+            for (list_i=0; list_i<tail; list_i++){
+              if (0==strcmp(syscall_info_list[list_i].name, name_buf)){
+                syscall_info_list[list_i].time += tmp_time;
+              }
+            }
+            if (list_i==tail){
+              strcpy(syscall_info_list[tail].name, name_buf);
+              syscall_info_list[tail].time = tmp_time;
+              tail++;
+            }
+            name_buf[0] = 0;
+            time_buf[0] = 0;
+            reading = 0;
+            pbuf += i+1;
+            if (time>0.001){
+              time = 0;
+              qsort(syscall_info_list, tail, sizeof(SyscallInfo), [](const void *a, const void *b) {
+                  return ((SyscallInfo *)b)->time - ((SyscallInfo *)a)->time;
+              });
+              printf("Top 5 syscalls by time:\n");
+              for (int i = 0; i < (tail < 5 ? tail : 5); i++) {
+                  printf("%s: %.6f seconds\n", syscall_info_list[i].name, syscall_info_list[i].time);
+              }
+            }
+          }
+      }else if (reading){ // 读完name还没开始读time
+        i = strstr(pbuf, "<");
+        if(i){
+          reading_time = 1;
+          pbuf += i+1;
+        }else{
+          pbuf = buf+num_read;
+        }
+      }else{
+        i = strstr(pbuf, "\n");
+        if (i){
+          reading = 1;
+          reading_name = 1;
+          pbuf += i+1;
+        }else{
+          pbuf = buf+num_read;
+        }
+      }
+    }
+
+// [ ] todo
+    
     // printf("[%d] Got: '%s'\n", getpid(), buf);
   }
   close(fd);
   return 0;
 }
 
-// [ ] todo
 // sperf CMD [ARG]  ->  strace CMD [ARG] | sperf
 int main(int argc, char *argv[]) {
   // 1. 判断调用形式
@@ -76,15 +156,6 @@ int main(int argc, char *argv[]) {
     close(pipefd[1]);
     sperf(pipefd[0]);
   }
-
-
-
-
-  // char *exec_argv[] = { "strace", "ls", NULL, };
-  // char *exec_envp[] = { "PATH=/bin", NULL, };
-  // execve("strace",          exec_argv, exec_envp);
-  // execve("/bin/strace",     exec_argv, exec_envp);
-  // execve("/usr/bin/strace", exec_argv, exec_envp);
   perror(argv[0]);
   exit(EXIT_FAILURE);
 }
